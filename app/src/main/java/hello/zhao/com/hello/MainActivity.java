ae35d8e82be3be83d32d2112342e11b63a27232f
zhaokaifeng1 <1739862519@qq.com>
2018-03-21 08:55:34 +0800

    Initial commit

---
+++ app/src/main/java/hello/zhao/com/hello/MainActivity.java
@@ -0,0 +1,14 @@
+package hello.zhao.com.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //TODO finish this code
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
